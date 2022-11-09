ce0f2b278f3111e37646571f562a7bb8df4de686
jo <chjo0330@gmail.com>
2022-11-09 16:28:43 +0900

    백엔드 기초 코드 추가

---
+++ lastboard_backend/src/main/java/com/ssafy/lastboard/LastboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.lastboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastboardBackendApplication.class, args);
+	}
+
+}
---
+++ lastboard_backend/src/test/java/com/ssafy/lastboard/LastboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.lastboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
