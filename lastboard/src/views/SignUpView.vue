0e533aab3c50d2bff2631646fb65fca90f3ca6ec
jo <chjo0330@gmail.com>
2022-11-09 16:11:55 +0900

    Front Component(SignUp, UserList) 작성

---
+++ lastboard/src/views/SignUpView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="regist">
+    <h1 class="underline">회원 가입</h1>
+    <div class="regist_form">
+      <label for="user_id">아이디</label>
+      <input type="text" v-model="user_id" ref="user_id" /><br />
+      <label for="user_password">비밀번호</label>
+      <input type="password" v-model="user_password" ref="user_password" /><br />
+      <label for="user_name">이름</label>
+      <input type="text" v-model="user_name" ref="user_name" /><br />
+      <label for="email_id">이메일</label>
+      <input type="text" v-model="email_id" ref="email_id" /><br />
+      <label for="email_domain">이메일 도메인</label>
+      <input type="text" v-model="email_domain" ref="email_domain" /><br />
+      <br />
+      <button @click="checkValue">등록</button>
+      <button @click="moveList">목록</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user_id: "",
+      user_password: "",
+      user_name: "",
+      email_id: "",
+      email_domain: "",
+    };
+  },
+  methods: {
+    //사용자 입력값 체크
+    checkValue() {
+      let err = true;
+      let msg = "";
+      if (!this.user_id) {
+        msg = "아이디를 입력해주세요";
+        err = false;
+        this.$refs.user_id.focus();
+      } else if (!this.user_password) {
+        msg = "비밀번호를 입력해주세요";
+        err = false;
+        this.$refs.user_password.focus();
+      } else if (!this.user_name) {
+        msg = "이름을 입력해주세요";
+        err = false;
+        this.$refs.user_name.focus();
+      } else if (!this.email_id) {
+        msg = "이메일을 입력해주세요";
+        err = false;
+        this.$refs.email_id.focus();
+      } else if (!this.email_domain) {
+        msg = "이메일 도메인을 입력해주세요";
+        err = false;
+        this.$refs.email_domain.focus();
+      }
+
+      if (!err) {
+        alert(msg);
+      } else {
+        this.signUp();
+      }
+    },
+    signUp() {},
+  },
+};
+</script>
+
+<style></style>
---
+++ lastboard/src/views/UserListView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1 class="underline">사용자 목록</h1>
+    <div style="text-align: right">
+      <button @click="movePage">회원가입</button>
+    </div>
+    <div v-if="users.length">
+      <table id="user-list">
+        <colgroup>
+          <col style="width: 5%" />
+          <col style="width: 10%" />
+          <col style="width: 10%" />
+          <col style="width: 15%" />
+          <col style="width: 20%" />
+          <col style="width: 20%" />
+          <col style="width: 20%" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>아이디</th>
+            <th>이름</th>
+            <th>비밀번호</th>
+            <th>이메일</th>
+            <th>이메일 도메인</th>
+            <th>가입날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in users" :key="index">
+            <td>{{ index + 1 }}</td>
+            <td>{{ user.user_id }}</td>
+            <td>{{ user.user_name }}</td>
+            <td>{{ user.user_password }}</td>
+            <td>{{ user.email_id }}</td>
+            <td>{{ user.email_domain }}</td>
+            <td>{{ user.join_date }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else class="text-center">등록된 사용자가 없습니다.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {},
+  methods: {
+    movePage() {
+      this.$router.push({ path: "/signup" });
+    },
+  },
+};
+</script>
+
+<style></style>
